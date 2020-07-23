277403f2cb4e1258b90707247fff202e795549e1
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-07-23 19:51:31 +1000

    Add support for saving and loading scans (#7)
    * Add save/load bindings and open/save file dialog boxes
    
    * Add scan saving and loading
    
    * Add scan loader
    
    * Condense isFirstFile and isRoot flags into a single flag
    
    * Fix issue where program would crash when attempting to display a deleted file
    
    * Fix typo
    
    * Force Deleted Item Error Icon To Larger Size
    
    * Add new display icons
    
    * Fix typo
    
    * Make requested changes
    
    Co-authored-by: RandomExplosion <benjaminjmcgaw@gmail.com>

--- Drive Scan/DriveScan.xaml.cs
+++ Drive Scan/DriveScan.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using Microsoft.Win32;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -23,6 +24,10 @@ namespace Drive_Scan
 
         //Singleton Pattern
         static DriveScanWindow currentWindow;
+
+        // Create an instance of the scan loader so we can keep track of the files that we have found
+        //  So that when the user goes to save/load a scan it has all the files ready to go
+        static ScanLoader currentScan = new ScanLoader();
 
         //Viewmodel for scanned folders and paths
         public ObservableCollection<FolderInfo> scannedDrives;
@@ -96,17 +101,21 @@ namespace Drive_Scan
                 //Get selected item from DirectoryTree
                 FileInfo item = DirectoryTree.SelectedItem as FileInfo;
 
-                //Get attributes for the selected item
-                FileAttributes attr = File.GetAttributes(item.path);
-
-                //If the selected item is a folder
-                if((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                // Make sure the file actually exists
+                if (File.Exists(item.path) || Directory.Exists(item.path))
                 {
-                    //Set the selected Folder to the selected item
-                    selectedFolder = DirectoryTree.SelectedItem as FolderInfo;
-                    
-                    //Update the FolderContentsView
-                    FolderContentsView.ItemsSource = selectedFolder.children;
+                    //Get attributes for the selected item
+                    FileAttributes attr = File.GetAttributes(item.path);
+
+                    //If the selected item is a folder
+                    if((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        //Set the selected Folder to the selected item
+                        selectedFolder = DirectoryTree.SelectedItem as FolderInfo;
+                        
+                        //Update the FolderContentsView
+                        FolderContentsView.ItemsSource = selectedFolder.children;
+                    }   
                 }
             }
         }
@@ -123,35 +132,39 @@ namespace Drive_Scan
                 //Get selected item from DirectoryTree
                 FileInfo item = DirectoryTree.SelectedItem as FileInfo;
 
-                //Get attributes for the selected item
-                FileAttributes attr = File.GetAttributes(item.path);
-
-                //If the selected item is a file
-                if((attr & FileAttributes.Directory) != FileAttributes.Directory)
+                // Make sure the file actually exists
+                if (File.Exists(item.path) || Directory.Exists(item.path))
                 {
-                    try
+                    //Get attributes for the selected item
+                    FileAttributes attr = File.GetAttributes(item.path);
+
+                    //If the selected item is a file
+                    if((attr & FileAttributes.Directory) != FileAttributes.Directory)
                     {
-                        //Open it in its associated application
-                        var p = new Process();
-                        p.StartInfo = new ProcessStartInfo(item.path)
-                        { 
-                            UseShellExecute = true
-                        };
-                        p.Start();
-                    }
-                    catch (Exception)
-                    {
-                        //User most likely attempted to open a file without an assigned application
-                        //So open it with the 'how do you want to open this file' dialog
-                        var p = new Process();
-                        p.StartInfo = new ProcessStartInfo(item.path)
+                        try
                         {
-                            WindowStyle = ProcessWindowStyle.Normal,
-                            Verb = "openas",
-                            UseShellExecute = true,
-                            ErrorDialog = true
-                        };
-                        p.Start();
+                            //Open it in its associated application
+                            var p = new Process();
+                            p.StartInfo = new ProcessStartInfo(item.path)
+                            { 
+                                UseShellExecute = true
+                            };
+                            p.Start();
+                        }
+                        catch (Exception)
+                        {
+                            //User most likely attempted to open a file without an assigned application
+                            //So open it with the 'how do you want to open this file' dialog
+                            var p = new Process();
+                            p.StartInfo = new ProcessStartInfo(item.path)
+                            {
+                                WindowStyle = ProcessWindowStyle.Normal,
+                                Verb = "openas",
+                                UseShellExecute = true,
+                                ErrorDialog = true
+                            };
+                            p.Start();
+                        }
                     }
                 }
             }
@@ -170,45 +183,49 @@ namespace Drive_Scan
                 //Get selected item from FolderContentsView
                 FileInfo item = FolderContentsView.SelectedItem as FileInfo;
 
-                //Get attributes for the selected item
-                FileAttributes attr = File.GetAttributes(item.path);
-
-                //If the selected item is a folder
-                if((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                // Make sure the file actually exists
+                if (File.Exists(item.path) || Directory.Exists(item.path))
                 {
-                    //Set the selected Folder to the selected item
-                    selectedFolder = FolderContentsView.SelectedItem as FolderInfo;
-                    
-                    //Update the FolderContentsView
-                    FolderContentsView.ItemsSource = selectedFolder.children;
-                }
+                    //Get attributes for the selected item
+                    FileAttributes attr = File.GetAttributes(item.path);
 
-                //This is a file
-                else
-                {
-                    try
+                    //If the selected item is a folder
+                    if((attr & FileAttributes.Directory) == FileAttributes.Directory)
                     {
-                        //Open it in its associated application
-                        var p = new Process();
-                        p.StartInfo = new ProcessStartInfo(item.path)
-                        { 
-                            UseShellExecute = true
-                        };
-                        p.Start();
+                        //Set the selected Folder to the selected item
+                        selectedFolder = FolderContentsView.SelectedItem as FolderInfo;
+                        
+                        //Update the FolderContentsView
+                        FolderContentsView.ItemsSource = selectedFolder.children;
                     }
-                    catch (Exception)
+
+                    //This is a file
+                    else
                     {
-                        //User most likely attempted to open a file without an assigned application
-                        //So open it with the 'how do you want to open this file' dialog
-                        var p = new Process();
-                        p.StartInfo = new ProcessStartInfo(item.path)
+                        try
                         {
-                            WindowStyle = ProcessWindowStyle.Normal,
-                            Verb = "openas",
-                            UseShellExecute = true,
-                            ErrorDialog = true
-                        };
-                        p.Start();
+                            //Open it in its associated application
+                            var p = new Process();
+                            p.StartInfo = new ProcessStartInfo(item.path)
+                            { 
+                                UseShellExecute = true
+                            };
+                            p.Start();
+                        }
+                        catch (Exception)
+                        {
+                            //User most likely attempted to open a file without an assigned application
+                            //So open it with the 'how do you want to open this file' dialog
+                            var p = new Process();
+                            p.StartInfo = new ProcessStartInfo(item.path)
+                            {
+                                WindowStyle = ProcessWindowStyle.Normal,
+                                Verb = "openas",
+                                UseShellExecute = true,
+                                ErrorDialog = true
+                            };
+                            p.Start();
+                        }
                     }
                 }
             }
@@ -233,15 +250,19 @@ namespace Drive_Scan
                 //Show the Bar
                 ProgBar.Visibility = Visibility.Visible;
                 //Scan the drive asynchronously then add the drive tree to the TreeView
-                Task scanTask = Task.Run(() => {Scanning.DirectoryScanner.FindFiles(drive.Name, OnFileFound);
+                Task scanTask = Task.Run(() => {
+                    Scanning.DirectoryScanner.FindFiles(drive.Name, file => {
+                        // Add the file to the scan
+                        currentScan.files.Add(file);
+                        OnFileFound(file);
+                    });
                 
                     //Add Drive tree to ui when finished then Release Working Resources (deallocate ram from _workingTree) when scan is finished
-                    Application.Current.Dispatcher.Invoke( () => 
+                    Application.Current.Dispatcher.Invoke(() => 
                     {
                         scannedDrives.Add(_workingTree.Value);  //Add drive to tree
                         ProgBar.Visibility = Visibility.Hidden; //Hide Progress Bar
                     });
-                
                 });
             }
         }
@@ -252,12 +273,10 @@ namespace Drive_Scan
         /// <param name="foundFile"></param>
         /// <param name="isFirstFile"></param>
         /// <param name="isRoot"></param>
-        public void OnFileFound(Scanning.File foundFile, bool isFirstFile, bool isRoot)
+        public void OnFileFound(Scanning.File foundFile)
         {
-            //Console.WriteLine(foundFile);
-
             //If this is the root folder
-            if (isRoot && isFirstFile)
+            if (foundFile.isFirstRoot)
             {
                 //If this is the second time this drive's root has been retuned (has the final size)
                 if (foundFile.size > 0)
@@ -321,6 +340,61 @@ namespace Drive_Scan
             {
                 ConfigHandler.updateValue("theme", "light");
                 ThemeManager.Current.ChangeTheme(Application.Current, "Light.Taupe", false);
+            }
+        }
+
+        /// <Summary>
+        /// Load a scan
+        /// </summary>
+        public void LoadScan(object Sender, RoutedEventArgs e)
+        {
+            // Configure the open file dialog box
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = "drsn";
+            dlg.Filter = "Scan Files (.drsn)|*.drsn";
+
+            // Show the dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Show the progress bar
+                ProgBar.Visibility = Visibility.Visible;
+
+                //Load the drives from the file asynchronously then add the drive tree to the TreeView
+                Task scanTask = Task.Run(() => {
+                    // Load the data from the file
+                    currentScan.Load(dlg.FileName);
+
+                    // Run the onfilefound function for each of the files found from the scanner load
+                    currentScan.files.ForEach(OnFileFound);
+                
+                    //Add Drive tree to ui when finished then Release Working Resources (deallocate ram from _workingTree) when scan is finished
+                    Application.Current.Dispatcher.Invoke(() => 
+                    {
+                        scannedDrives.Add(_workingTree.Value);  //Add drive to tree
+                        ProgBar.Visibility = Visibility.Hidden; //Hide Progress Bar
+                    });
+                });
+            }
+        }
+
+        /// <Summary>
+        /// Save the current scan
+        /// </summary>
+        public void SaveScan(object Sender, RoutedEventArgs e)
+        {
+            // Configure the save file dialog box
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = ".drsn";
+            dlg.Filter = "Scan Files (.drsn)|*.drsn";
+
+            // Show the dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                currentScan.Save(dlg.FileName);
             }
         }
 
